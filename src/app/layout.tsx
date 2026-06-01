d52afbbf8a765d0300c2aa0f96d7461c12264f3e
Lancesic <lancebradleyadcock@gmail.com>
2026-06-01 12:06:56 -0400

    Load hard signup personalization guard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { DashboardOpenEstimates } from "@/components/dashboard-open-estimates";
 import { DashboardSpacingPolish } from "@/components/dashboard-spacing-polish";
 import { ScriptTemplateUpgrade } from "@/components/script-template-upgrade";
 import { SettingsPolish } from "@/components/settings-polish";
+import { SignupHardPersonalizer } from "@/components/signup-hard-personalizer";
 import { SignupPersonalizer } from "@/components/signup-personalizer";
 import { SignupSettingsBridge } from "@/components/signup-settings-bridge";
 import "./globals.css";
@@ -44,6 +45,7 @@ export default function RootLayout({
         <SettingsPolish />
         <SignupSettingsBridge />
         <SignupPersonalizer />
+        <SignupHardPersonalizer />
         {children}
       </body>
     </html>
